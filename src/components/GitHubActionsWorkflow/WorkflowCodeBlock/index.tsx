707ff05709c50db742155e32a2cd569f5f0a0941
Mikael Andersson <mikael.l.andersson91@gmail.com>
2025-07-28 11:22:33 +0200

    fix(WorkflowCodeBlock): add optional properties for repo
    Add optional properties for repository, owner and version for displaying
    a specific version of the code

--- src/components/GitHubActionsWorkflow/WorkflowCodeBlock/index.tsx
+++ src/components/GitHubActionsWorkflow/WorkflowCodeBlock/index.tsx
@@ -3,11 +3,16 @@ import CodeBlock from '@theme/CodeBlock'
 
 interface WorkflowCodeBlockProps {
   workflow: string
+  owner?: string
+  repository?: string
+  version?: string
 }
 
 const WorkflowCodeBlock: React.FC<WorkflowCodeBlockProps> = (props) => {
+  const owner = props.owner || 'mikael-andersson91';
+  const version = props.version || 'main';
   const wf = require('raw-loader!@site/.github/workflows/' + props.workflow).default
-  const workflowLink = `https://github.com/mikael-andersson91/rpa-devops/blob/main/.github/workflows/${props.workflow}`
+  const workflowLink = `https://github.com/${owner}/${props.repository}/blob/${version}/.github/workflows/${props.workflow}`
   return (
     <CodeBlock language="yml" title=<a href={workflowLink}>{props.workflow}</a> showLineNumbers={true}>
       {wf}
